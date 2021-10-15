cf67cc9e24a7ef6ba97915140f07481c6005a880
Andrea Ho <anhhothai.iamcoder@gmail.com>
2021-10-15 10:17:39 +0700

    Add the WebSocket-based log viewer window

---
+++ public/javascripts/log-viewer.js
@@ -0,0 +1,15 @@
+// Create a WebSocket client connection object
+// WebSocket is a browser-support web API
+const connection = new WebSocket('ws://localhost:3000')
+
+// Get a reference to the DOM element #log-window
+const logWindow = document.querySelector('#log-window')
+
+// Send a message to WebSocket
+connection.onopen = function () {
+    connection.send('Hello from the client')
+
+    connection.onmessage = function (event) {
+        logWindow.innerHTML = `<p>${event.data}</p>`
+    }
+}
